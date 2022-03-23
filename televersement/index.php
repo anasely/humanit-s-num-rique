0b43cf1e46771a87a14642416cb5facc04b91675
anasely <58348015+anasely@users.noreply.github.com>
2022-03-23 14:05:55 +0100

    upload normal vers un dossier photo

--- televersement/index.php
+++ televersement/index.php
@@ -1,37 +1,58 @@
 <?php
-
-    // if le fichier est bien importé
-    if(isset($_FILES['file'])) {
-        // Connexion à la base de données
-        $pdo = new PDO('mysql:host=localhost;dbname=televersement','root','root');
+    // verification de l'importation de fichier
+    if(isset($_FILES['fichier'])) {
+        // Connexion à la bdd
+        $pdo = new PDO('mysql:host=localhost;dbname=tps_php', 'root', 'root');
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        
-        // Récupérer les informations du fichier importé        
-        $nom = $_FILES['file']['name'];
-        $size = $_FILES['file']['size'];
+        // Récupérer les info du fichier importé        
+        $nom = $_FILES['fichier']['name'];
+        $size = $_FILES['fichier']['size'];
         $path = 'photo/' . $nom;
- 
+        //verification de la taille maximale d'un fichier
+        $maxsize = 8*1024*1024;
+
+        //Les extensions acceptées
+        $lesextension = ["png", "jpeg", "jpg", "pjpeg"];
+
+        //Récupérer l'extension du fichier importé
+        $extension = pathinfo($nom, PATHINFO_EXTENSION);
+
+        //Vérifier l'extension du fichier
+        if(in_array($extension, $lesextension)) {
             
-          
-             //Vérifier si le fichier existe
-                if(file_exists("photo/" . $_FILES['file']['name'])) {
-                    die("Ce fichier est deje exist.");
+            //Vérifier la taille du fichier
+            if($size > $maxsize) {
+                die("La taille du fichier dépasse la taille autorisée");
+            } 
+            else {
+                //Vérifier si le fichier existe
+                if(file_exists("photo/" . $_FILES['fichier']['name'])) {
+                    die("Ce fichier existe déjà.");
                 }
-               
-                    //Déplacer le fichier vers le dossier images
-                    if(move_uploaded_file($_FILES['file']['tmp_name'], "photo/". $_FILES['file']['name'])) {
-                        $query = "INSERT INTO fichiers (nom, type, path) VALUES (?, ?, ?)";
+                else {
+
+                    //Déplacer le fichier vers le dossier photo
+                    if(move_uploaded_file($_FILES['fichier']['tmp_name'], "photo/". $_FILES['fichier']['name'])) {
+                        $query = "INSERT INTO fichiers (nom, type, path, size) VALUES (?, ?, ?, ?)";
                         $statement = $pdo->prepare($query);
-                        $statement->execute(array($nom, $path));
-                        echo "Le téléchargement done";
+                        $statement->execute(array($nom, $extension, $path, $size));
+                        echo "Le téléchargement ok";
                     } 
-                
                     else {
-                        die("Le téléchargement a echoué");
+                        die("Le téléchargement a échoué");
                     }
-                    
-      
+                }
+            }
+        }
+        else {
+            die("le fichier dois etre soit PNG, JPEG, JPG ou PJPEG");
+        }
     }
 ?>
+
+
 
 <!DOCTYPE html>
 <html>
@@ -41,8 +62,8 @@
     </head>
     <body>
         <form action="index.php" method="POST" enctype="multipart/form-data">
-            <input type="file" name="file" id="file">
-            <input type="submit" value="ok" name="submit"/>
+            <input type="file" name="fichier">
+            <input type="submit" value="Envoyer" name="submit"/>
         </form>
     </body>
     
